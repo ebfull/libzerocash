daa2b079710db827a0a02e10e6ba58e5deb4b823
Taylor Hornby <havoc@defuse.ca>
2015-12-07 12:10:14 -0700

    Add parentheses around arithmetic expressions in macros to avoid bugs.

--- libzerocash/Zerocash.h
+++ libzerocash/Zerocash.h
@@ -23,11 +23,11 @@
 #define a_pk_size       32
 #define pk_enc_size     311
 #define sig_pk_size		32
-#define addr_pk_size    a_pk_size+pk_enc_size
+#define addr_pk_size    (a_pk_size+pk_enc_size)
 
 #define a_sk_size       32
 #define sk_enc_size     287
-#define addr_sk_size    a_sk_size+sk_enc_size
+#define addr_sk_size    (a_sk_size+sk_enc_size)
 
 #define v_size          8
 #define rho_size        32
@@ -35,8 +35,8 @@
 #define s_size          0
 #define k_size          32
 #define cm_size         32
-#define coin_size       addr_pk_size+v_size+rho_size+r_size+s_size+cm_size
-#define tx_mint_size    cm_size+v_size+k_size+s_size
+#define coin_size       (addr_pk_size+v_size+rho_size+r_size+s_size+cm_size)
+#define tx_mint_size    (cm_size+v_size+k_size+s_size)
 
 #define root_size       32
 #define sn_size         32
@@ -45,7 +45,7 @@
 #define zerocash_pour_proof_size 288
 #define C_size          173
 #define sigma_size      72
-#define tx_pour_size    root_size+(2*sn_size)+(2*cm_size)+v_size+pk_sig_size+(2*h_size)+zerocash_pour_proof_size+(2*C_size)+sigma_size
+#define tx_pour_size    (root_size+(2*sn_size)+(2*cm_size)+v_size+pk_sig_size+(2*h_size)+zerocash_pour_proof_size+(2*C_size)+sigma_size)
 
 #define SNARK
 
